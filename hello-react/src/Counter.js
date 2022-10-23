b81e37d18aa1192507c979b1f84999e17828e103
lisa3999 <lisa48625@khu.ac.kr>
2022-10-23 16:38:14 +0900

    Add Say.js

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,14 +1,10 @@
 import {Component} from 'react';
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        //state 초기값 설정하기
-        this.state = {
-            number: 0,
-            fixednumber: 0
-        };
-    }
+    state = {
+        number: 0,
+        fixednumber: 0
+    };
     render () {
         const { number, fixednumber } = this.state;
         return (
@@ -17,7 +13,15 @@ class Counter extends Component {
                 <h2>바뀌지 않는 값: {fixednumber}</h2>
                 <button
                     onClick={()=>{
-                        this.setState({number: number+1});
+                        this.setState(
+                            {
+                                number: number+1
+                            },
+                            ()=> {
+                                console.log('방금 setState가 호출되었습니다.');
+                                console.log(this.state);
+                            }
+                        );
                     }}
                 >
                     +1
